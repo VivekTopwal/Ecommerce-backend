a3744387fd61219d574a1539ac6b48dbbc359a08
VivekTopwal <rohit.kumar@devouttechconsultants.com>
2026-03-24 15:31:25 +0530

    Minor changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,8 +73,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
-
+    
     if (!email || !password) {
       return res.status(400).json({
         success: false,
